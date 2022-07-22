dec30ec7f54275372ea6c49cbeed7627d4ee57c1
celsopires1999 <celso.pires1999@gmail.com>
2022-07-22 00:00:24 +0000

    create with validation

--- src/transactions/dto/create-transaction.dto.ts
+++ src/transactions/dto/create-transaction.dto.ts
@@ -1,13 +1,38 @@
+import {
+  IsIn,
+  IsISO8601,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+import {
+  TransactionCategoryList,
+  TransactionTypeList,
+} from '../entities/transaction.entity';
+
 export class CreateTransactionDto {
+  @IsISO8601()
+  @IsNotEmpty()
   payment_date: Date;
 
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
   name: string;
 
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
   description: string;
 
+  @IsIn(TransactionCategoryList)
+  @IsNotEmpty()
   category: string;
 
+  @IsNotEmpty()
   amount: number;
 
+  @IsIn(TransactionTypeList)
+  @IsNotEmpty()
   type: string;
 }
